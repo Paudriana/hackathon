af8bf7f52f5bac19f8042013f6da54a7fdfdc20c
Paudriana <137229196+Paudriana@users.noreply.github.com>
2025-03-28 00:52:29 -0500

    Agregando estilos

--- src/pages/avatar/CreateAvatar.jsx
+++ src/pages/avatar/CreateAvatar.jsx
@@ -4,15 +4,17 @@ import styles from "./Avatar.module.scss";
 import { useNavigate } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-
 const ACTIONS = {
   SET_AVATAR: "SET_AVATAR",
+  SET_NAME: "SET_NAME",
 };
 
 const avatarReducer = (state, action) => {
   switch (action.type) {
     case ACTIONS.SET_AVATAR:
       return { ...state, ...action.payload };
+    case ACTIONS.SET_NAME:
+      return { ...state, name: action.payload };
     default:
       return state;
   }
@@ -25,6 +27,7 @@ const initialAvatarState = {
   clotheType: "ShirtCrewNeck",
   clotheColor: "PastelBlue",
   skinColor: "Light",
+  name: "",
 };
 
 function CreateAvatar() {
@@ -34,16 +37,20 @@ function CreateAvatar() {
   const avatars = [
     { topType: "LongHairCurly", accessoriesType: "Prescription02", hairColor: "Auburn", clotheType: "ShirtCrewNeck", clotheColor: "PastelBlue", skinColor: "Light" },
     { topType: "LongHairBob", accessoriesType: "Blank", hairColor: "Red", clotheType: "ShirtCrewNeck", clotheColor: "PastelGreen", skinColor: "Light" },
-    { topType: "LongHairStraightStrand", accessoriesType: "Blank", hairColor: "Black", clotheType: "ShirtCrewNeck", clotheColor: "Pink", skinColor: "Light" },
+    { topType: "LongHairStraightStrand", accessoriesType: "Blank", hairColor: "Black", clotheType: "ShirtCrewNeck", clotheColor: "Pink", skinColor: "Pale" },
     { topType: "LongHairNotTooLong", accessoriesType: "Prescription02", hairColor: "BrownDark", clotheType: "ShirtCrewNeck", clotheColor: "Heather", skinColor: "Light" },
-    { topType: "LongHairBun", accessoriesType: "Blank", hairColor: "BrownDark", clotheType: "ShirtCrewNeck", clotheColor: "PastelOrange", skinColor: "Light" },
+    { topType: "LongHairBun", accessoriesType: "Blank", hairColor: "BrownDark", clotheType: "ShirtCrewNeck", clotheColor: "PastelOrange", skinColor: "Brown" },
     { topType: "LongHairBigHair", accessoriesType: "Blank", hairColor: "Blonde", clotheType: "ShirtCrewNeck", clotheColor: "Red", skinColor: "Light" },
-    { topType: "LongHairCurvy", accessoriesType: "Round", hairColor: "Black", clotheType: "ShirtCrewNeck", clotheColor: "White", skinColor: "Light" },
-    { topType: "LongHairFroBand", accessoriesType: "Blank", hairColor: "BrownDark", clotheType: "ShirtCrewNeck", clotheColor: "PastelRed", skinColor: "Light" },
+    { topType: "LongHairCurvy", accessoriesType: "Round", hairColor: "Black", clotheType: "ShirtCrewNeck", clotheColor: "Black", skinColor: "Light" },
+    { topType: "LongHairFroBand", accessoriesType: "Blank", hairColor: "BrownDark", clotheType: "ShirtCrewNeck", clotheColor: "PastelRed", skinColor: "DarkBrown" },
   ];
 
   const handleSelectAvatar = (avatar) => {
     dispatch({ type: ACTIONS.SET_AVATAR, payload: avatar });
+  };
+
+  const handleNameChange = (event) => {
+    dispatch({ type: ACTIONS.SET_NAME, payload: event.target.value });
   };
 
   const handleContinue = () => {
@@ -51,30 +58,50 @@ function CreateAvatar() {
   };
 
   return (
-    <div>
-      <h2>Selecciona tu avatar</h2>
+    <>
+      <i className="bi bi-arrow-left" style={{ fontSize: "2rem" }}></i>
+      <div>
+        <h2 className={styles.avatarTitle}>Selecciona tu avatar</h2>
 
-      <div className={styles.selectedAvatar}>
-        <AvatarCard cardStyles={{ width: "200px", height: "200px" }} {...avatarState} />
-      </div>
+        <div className={styles.selectedAvatar}>
+          <AvatarCard cardStyles={{ width: "150px", height: "150px" }} {...avatarState} />
+        </div>
 
-      <div className={styles.avatarsContainer}>
-        {avatars.map((avatar, index) => (
-          <div key={index} onClick={() => handleSelectAvatar(avatar)}>
-            <AvatarCard {...avatar} />
+        <div className={styles.avatarsContainer}>
+          {avatars.map((avatar, index) => (
+            <div key={index} onClick={() => handleSelectAvatar(avatar)}>
+              <AvatarCard {...avatar} />
+            </div>
+          ))}
+          <div className={styles.randomAvatar} onClick={() => handleSelectAvatar({
+            topType: avatars[Math.floor(Math.random() * avatars.length)].topType,
+            accessoriesType: avatars[Math.floor(Math.random() * avatars.length)].accessoriesType,
+            hairColor: avatars[Math.floor(Math.random() * avatars.length)].hairColor,
+            clotheType: avatars[Math.floor(Math.random() * avatars.length)].clotheType,
+            clotheColor: avatars[Math.floor(Math.random() * avatars.length)].clotheColor,
+            skinColor: avatars[Math.floor(Math.random() * avatars.length)].skinColor,
+          })}>
+            <i className="bi bi-shuffle" style={{ fontSize: "2.5rem" }}></i>
           </div>
-         
-        ))}
-         <div className={styles.randomAvatar} onClick={() => handleSelectAvatar(avatars[Math.floor(Math.random() * avatars.length)])}>
-         <i className="bi bi-shuffle" style={{ fontSize: "2.5rem" }}></i></div>
-      </div>
+        </div>
 
-      <div className={styles.buttonContainer}>
-        <button type="button" className={styles.continueButton} onClick={handleContinue}>
-          Continuar
-        </button>
+        <div className={styles.inputContainer}>
+          <h2 className={styles.avatarTitle}>Ingresa tu nombre:</h2>
+          <input 
+            type="text" 
+            className={styles.nameInput} 
+            value={avatarState.name} 
+            onChange={handleNameChange}
+          />
+        </div>
+
+        <div className={styles.buttonContainer}>
+          <button type="button" className={styles.continueButton} onClick={handleContinue}>
+            Continuar
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/pages/intro/Intro.jsx
+++ src/pages/intro/Intro.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./Intro.module.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import introImg from "../../assets/intro.png";
+import logoImg from "../../assets/logo.png";
 
 function Intro() {
   const [showAnimation, setShowAnimation] = useState(true);
@@ -16,17 +18,22 @@ function Intro() {
     <div className={styles.introContainer}>
       {showAnimation ? (
         <div className={styles.logoAnimation}>
-          <img src="/logo.png" alt="Logo Animado" className={styles.logo} />
+          <img src={logoImg} alt="Logo" className={styles.logo} />
         </div>
       ) : (
-        <div className={styles.introContent}>
-          <img src="/intro-image.png" alt="Bienvenida" className={styles.introImage} />
-          <h1>¡Bienvenida a la aventura financiera!</h1>
-          <p>Aprende a manejar tus finanzas de manera divertida y sencilla.</p>
-          <button className="btn btn-primary" onClick={() => navigate("/avatar")}>
+        <div>
+          <div className={styles.introContent}>
+            <img src={introImg} alt="Bienvenida" className={styles.introImage} />
+            <h1>Punche</h1>
+            <h2>Educación financiera</h2>
+            <p>Te ofrecemos herramientas para manejar mejor tu dinero, tomando decisiones que te permitan crecer, de manera divertida y sencilla</p>
+
+          </div>
+          <button className={styles.introButton} onClick={() => navigate("/avatar")}>
             Comencemos!
           </button>
         </div>
+
       )}
     </div>
   );
